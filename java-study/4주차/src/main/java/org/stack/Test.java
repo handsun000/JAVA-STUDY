9710505d6497f7218fcefec90977633df928985b
JinYoung, Son <handsun000@gmail.com>
2023-04-07 14:23:22 +0900

    stack

---
+++ java-study/4주차/src/main/java/org/stack/Test.java
@@ -0,0 +1,11 @@
+package main.java.org.stack;
+
+public class Test {
+    public static void main(String[] args) {
+        ListNodeStack st = new ListNodeStack();
+
+        for (int i = 0; i < 10; i++) {
+            st.push(i);
+        }
+    }
+}
